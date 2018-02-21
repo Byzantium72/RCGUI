6d41ba740c73b09dc96b761b90f708867b322ec8
Jase Rost <jaserost@yahoo.com>
2018-02-21 11:42:21 -0700

    some improvements

--- app/src/main/java/com/example/jase/prototype/connect.java
+++ app/src/main/java/com/example/jase/prototype/connect.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class connect extends AppCompatActivity {
@@ -45,7 +49,29 @@ public class connect extends AppCompatActivity {
         });
     }
 
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length() - 17);
+            Intent intent = new Intent(connect.this, gui.class);
+            intent.putExtra("Extra Address", address);
+            startActivity(intent);
+        }
+    };
     public void pairedDevicesList(){
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList l = new ArrayList();
 
+        if(pairedDevices.size() > 0){
+            for(BluetoothDevice bt : pairedDevices){
+                l.add(bt.getName() + "\n" + bt.getAddress());
+            }
+        }else{
+            Toast.makeText(getApplicationContext(), "No paired Bluetooth devices", Toast.LENGTH_SHORT);
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,l);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(myListClickListener);
     }
 }
--- app/src/main/java/com/example/jase/prototype/gui.java
+++ app/src/main/java/com/example/jase/prototype/gui.java
@@ -1,13 +1,81 @@
 package com.example.jase.prototype;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class gui extends AppCompatActivity {
+
+    String address = null;
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gui);
+    }
+
+    private void msg(String s){
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void> {
+        private boolean ConnectSuccess = true;
+
+        @Override
+        protected void onPreExecute()
+        {
+            progress = ProgressDialog.show(gui.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+            try
+            {
+                if (btSocket == null || !isBtConnected)
+                {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();//start connection
+                }
+            }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess)
+            {
+                msg("Connection Failed.");
+                finish();
+            }
+            else
+            {
+                msg("Connected to " + myBluetooth.getName());
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
     }
 }
